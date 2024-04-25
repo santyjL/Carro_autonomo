5781700de3cf18c109f0838be44c303224ec74e3
santyjL <jlopezsanty1@gmail.com>
2024-04-25 11:10:54 -0600

    en el codigo se añade un segundo sensor que es para medir el vacio para evitar que el carro autonomo caiga en caso de estar en una altura ralativamente alta

--- CARRO_QUE_EVITA_OBSTACULOS.ino
+++ CARRO_QUE_EVITA_OBSTACULOS.ino
@@ -1,27 +1,31 @@
-/*ALGOBACK YOUTUBE CHANNEL*/
 /*CODIGO PARA CARRO QUE EVITA OBSTACULOS */
-
-#include <Servo.h> 
+// importaciones para el manejo de los motores y el sensor
+#include <Servo.h>
 #include <NewPing.h>
-#include <AFMotor.h>  
+#include <AFMotor.h>
 
-NewPing Dist(A0, A1, 200); //(trig,echo,dist max)
-Servo myServo; 
+NewPing Dist(A0, A1, 300); //(trig,echo,dist max)
+NewPing Dist2(A2 , A3 , 50); //(trig , echo , dist max)
+Servo myServo;
 
+// definicion de las variables de distancia
+int distanciaVacio = 0;
 int distancia = 0;
 int distanciaD = 0;
 int distanciaIz = 0;
 boolean Booleano = false;
 
-AF_DCMotor Motor1(1, MOTOR12_1KHZ); 
+//Definicion de los motores
+AF_DCMotor Motor1(1, MOTOR12_1KHZ);
 AF_DCMotor Motor2(2, MOTOR12_1KHZ);
 AF_DCMotor Motor3(3, MOTOR34_1KHZ);
 AF_DCMotor Motor4(4, MOTOR34_1KHZ);
 
 void setup() {
-  myServo.attach(10);  
+  Serial.begin(9600);
+  myServo.attach(10);
   myServo.write(115);
-  delay(1000); 
+  delay(1000);
   myServo.write(65);
   delay(500);
   myServo.write(160);
@@ -31,114 +35,132 @@ void setup() {
 
 void loop() {
 distancia = medirDistancia();
-if (distancia >= 25 && distancia <= 250)
-{
-  Adelante();
-}
-else if (distancia < 25)
-{
-  Frenar();
-  delay(150);
-  Reversa();
-  delay(250);
-  Frenar();
-  delay(250);
-  distanciaD = mirarDerecha();
-  delay(250);
-  distanciaIz = mirarIzquierda();
-  delay(250);
+distanciaVacio = medirDistanciaVacio();
+Serial.println(distanciaVacio);
 
-  if(distanciaD >= distanciaIz)
-    {
-      GirarDerecha();
-      Frenar();
-    }
+if (distancia >= 25 && distancia <= 300 && distanciaVacio <= 5)
+  {
+    Adelante();
+  }
+
+else if (distancia <= 25 || distanciaVacio >= 6)
+  {
+    Frenar();
+    delay(200);
+    Reversa();
+    delay(250);
+    Frenar();
+    delay(250);
+    distanciaD = mirarDerecha();
+    delay(250);
+    distanciaIz = mirarIzquierda();
+    delay(250);
+
+    if(distanciaD >= distanciaIz)
+      {
+        GirarDerecha();
+        Frenar();
+      }
+
     else if(distanciaIz >= distanciaD)
-    {
-      GirarIzquierda();
-      Frenar();
+      {
+        GirarIzquierda();
+        Frenar();
+      }
     }
   }
-}
+
 
 int mirarDerecha()
-{
-    myServo.write(60); 
-    delay(350);
-    int distancia = medirDistancia();
-    delay(50);
-    myServo.write(115);
-    return distancia;
-}
+  {
+      myServo.write(60);
+      delay(350);
+      int distancia = medirDistancia();
+      delay(50);
+      myServo.write(115);
+      return distancia;
+  }
 
 int mirarIzquierda()
+  {
+      myServo.write(165);
+      delay(350);
+      int distancia = medirDistancia();
+      delay(50);
+      myServo.write(115);
+      return distancia;
+  }
+
+int medirDistanciaVacio()
 {
-    myServo.write(165); 
-    delay(350);
-    int distancia = medirDistancia();
-    delay(50);
-    myServo.write(115); 
-    return distancia;
+  delay(50);
+  int distanciaVacio = Dist2.ping_cm();
+  if (distanciaVacio <= 0 || distanciaVacio >= 50)
+  {
+    distanciaVacio = 50;
+  }
+  return distanciaVacio;
 }
 
-  int medirDistancia() 
-  { 
+int medirDistancia()
+  {
     delay(50);
     int ditanciaCM = Dist.ping_cm();
-    if(ditanciaCM <= 0 || ditanciaCM >= 250)
+    if(ditanciaCM <= 0 || ditanciaCM >= 300)
     {
-      ditanciaCM = 250;
+      ditanciaCM = 300;
     }
     return ditanciaCM;
   }
 
-void Frenar() {
-  Motor1.run(RELEASE); 
-  Motor2.run(RELEASE);
-  Motor3.run(RELEASE);
-  Motor4.run(RELEASE);
- } 
-  
+void Frenar() 
+ {
+    Motor1.run(RELEASE);
+    Motor2.run(RELEASE);
+    Motor3.run(RELEASE);
+    Motor4.run(RELEASE);
+ }
+
 void Adelante() {
  if(Booleano == false)
   {
     Booleano = true;
-    Motor1.run(BACKWARD);      
+    Motor1.run(BACKWARD);
     Motor2.run(BACKWARD);
-    Motor3.run(BACKWARD); 
-    Motor4.run(BACKWARD);     
-    controlVelocidad();
+    Motor3.run(BACKWARD);
+    Motor4.run(BACKWARD);
+    controlVelocidad(150);
   }
 }
 
 void Reversa() {
     Booleano = false;
-    Motor1.run(FORWARD);      
+    Motor1.run(FORWARD);
     Motor2.run(FORWARD);
     Motor3.run(FORWARD);
-    Motor4.run(FORWARD);  
-    controlVelocidad();
+    Motor4.run(FORWARD);
+    controlVelocidad(220);
     delay(200);
-}  
+}
 
 void GirarDerecha() {
   Motor1.run(BACKWARD);
   Motor2.run(BACKWARD);
   Motor3.run(RELEASE);
-  Motor4.run(RELEASE);     
-  delay(500);  
-} 
- 
-void GirarIzquierda() {
-  Motor1.run(RELEASE);     
-  Motor2.run(RELEASE);  
-  Motor3.run(BACKWARD);
-  Motor4.run(BACKWARD);   
+  Motor4.run(RELEASE);
   delay(500);
-} 
+}
 
- void controlVelocidad(){
-   for (int velocidad = 0; velocidad < 160; velocidad +=2) 
+void GirarIzquierda() {
+  Motor1.run(RELEASE);
+  Motor2.run(RELEASE);
+  Motor3.run(BACKWARD);
+  Motor4.run(BACKWARD);
+  delay(500);
+}
+
+void controlVelocidad(int velocidad_max){
+  for (int velocidad = 0; velocidad < velocidad_max; velocidad +=2)
    {
       Motor1.setSpeed(velocidad);
       Motor2.setSpeed(velocidad);
